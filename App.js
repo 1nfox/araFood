567090d8d0e1624d0f5d3acf84819d847356abee
1nfox <screamin.littlerom@gmail.com>
2017-11-21 10:44:25 +0100

    Ajout de react-navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { TabNavigator } from 'react-navigation'
 
 
 
