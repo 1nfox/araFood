81683c46f9c947afab5a144eeb2fabc0874297ba
1nfox <screamin.littlerom@gmail.com>
2017-11-21 10:51:58 +0100

    test

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { TabNavigator } from 'react-navigation'
 
+import Home from './components/Home'
+import Search from './components/Profil'
+
+
+import { TabNavigator } from 'react-navigation'
+const Tabs = TabNavigator({
+  Search: { screen: Home },
+  Profil: { screen: Profil }
+}, {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
+    indicatorStyle: {
+      height: 2,
+      backgroundColor: '#FFFFFF'
+    },
+    style: {
+      backgroundColor: "#c0392b",
+      borderTopWidth: 1,
+      borderColor: "#3f101c"
+    }
+  },
+})
 
 
 import * as firebase from 'firebase'
-
 const firebaseConfig = {
   apiKey: "AIzaSyAl88Ba1yzhJjQ9AgA-Yzlo9V58vKWVAi4",
   authDomain: "ara-food.firebaseapp.com",
