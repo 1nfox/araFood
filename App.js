4ed6230dc8195010133ef4f9743cc538a7df7c35
1nfox <screamin.littlerom@gmail.com>
2017-11-21 10:31:52 +0100

    First

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAl88Ba1yzhJjQ9AgA-Yzlo9V58vKWVAi4",
+  authDomain: "ara-food.firebaseapp.com",
+  databaseURL: "https://ara-food.firebaseio.com",
+  storageBucket: "gs://ara-food.appspot.com",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
