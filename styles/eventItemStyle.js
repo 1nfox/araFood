70b269a7f1949aed2caf2c79bdad0a44530668a9
kek4 <belmont.julien.dev@gmail.com>
2017-12-18 17:14:46 +0100

    modif css, commentaire

--- styles/eventItemStyle.js
+++ styles/eventItemStyle.js
@@ -15,5 +15,33 @@ export default {
         justifyContent: 'space-around',
         paddingTop: 5,
         paddingBottom: 5
+    },
+    date: {
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        height: 'auto',
+        padding: 10,
+        fontSize:16,
+        color: '#FFF',
+        marginTop: 10,
+        marginLeft: 10
+    },
+    hour: {
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+        height: 'auto',
+        padding: 10,
+        fontSize:16,
+        marginTop: 10,
+        right: 10,
+        color: '#D92719'
+    },
+    count: {
+        height: 'auto',
+        backgroundColor: '#333',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 }
